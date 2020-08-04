2fe9290f04af2c3cb1e1694859a2ec0102333536
YY <z3388638@gmail.com>
2020-08-05 00:00:48 +0800

    Render the button w/ logic

---
+++ meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         package-finder
+// @namespace    https://2yc.tw
+// @version      0.0.1
+// @description  Userscript to find npm package in GitHub
+// @author       YY
+// @match        https://github.com/*
+// @license      MIT
+// @homepage     https://github.com/x3388638/package-finder
+// @grant        none
+// ==/UserScript==
--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,1 +1,22 @@
-<div>hello</div>
+<script>
+  import SearchIcon from './SearchIcon'
+
+  const regex = /"name\s([\w-_]+)"\sfilename:package\.json/
+  const SearchInput = document.querySelector('.js-jump-to-field')
+  const handleClick = () => {
+    let keyword = SearchInput.value
+    if (keyword) {
+      const isMatchRegex = keyword.match(regex)
+      if (isMatchRegex) {
+        keyword = isMatchRegex[1]
+      }
+
+      window.location.href = `/search?q="name ${keyword}"+filename:package.json`
+    }
+  }
+</script>
+
+<button class="btn btn-sm mr-5 f6" on:click={handleClick}>
+  <SearchIcon />
+  Package
+</button>
