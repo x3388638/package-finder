bd473c6b708e0377181f5ce5626cd63cf3962fb9
YY <z3388638@gmail.com>
2020-08-05 23:55:30 +0800

    Update userscript meta

--- meta.js
+++ meta.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         package-finder
 // @namespace    https://2yc.tw
-// @version      0.0.1
+// @version      0.1.0
 // @description  Userscript to find npm package in GitHub
 // @author       YY
 // @match        https://github.com/*
 // @license      MIT
 // @homepage     https://github.com/x3388638/package-finder
+// @updateURL    https://openuserjs.org/meta/x3388638/package-finder.meta.js
 // @grant        none
 // ==/UserScript==
